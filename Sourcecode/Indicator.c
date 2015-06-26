1dc64989295dcfdbea4e6ea8d8105c690cd10604
Luke Halley <luke123halley@gmail.com>
2015-06-26 22:22:23 +1000

    Add error handling notes

--- Sourcecode/Indicator.c
+++ Sourcecode/Indicator.c
@@ -85,3 +85,4 @@ void loop()
 // Return error code if indicator value exceeds maximum threshold
 // TODO: Optimize indicator update frequency for lower CPU usage
 // Returns indicator state code or negative error value
+// Check for null pointer and bounds violations
